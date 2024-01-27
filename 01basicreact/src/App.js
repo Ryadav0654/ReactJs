a9d894b29d59634f42f70f80deaeb1693db5b6d7
Ravindra Yadav <yadavra2005@gmail.com>
2024-01-28 00:51:48 +0530

    update commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>This is my first react app using create-react-app</h1>
+    </>
+  );
+}
+
+export default App;
