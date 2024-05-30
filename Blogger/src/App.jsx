81e4bf0b0e54cb154373edc9d569bd594213db63
Ravindra Yadav <yadavra2005@gmail.com>
2024-05-31 00:14:24 +0530

    authSlice and store

--- Blogger/src/App.jsx
+++ Blogger/src/App.jsx
@@ -1,18 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './Appwrite/auth'
 import './App.css'
+import { login, logout } from './Store/authSlice'
+import { Footer, Header } from './Components'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+  
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}));
+      }else{
+        dispatch(logout());
+      }
+    }).finally(() => setLoading(false))
+  }, [])
 
-  return (
-    <>
-      <h1>A Blogger website with appwrite.</h1>
-    </>
-  )
+
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return !loading ? 
+  (<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        {/* {outlet} */}
+        <h1 className='text-3xl font-bold '>A Blogger website with appwrite.</h1>
+      </main>
+      <Footer/>
+    </div>
+    
+  </div> )
+  : <h1>Loading...</h1>;
 }
 
 export default App
